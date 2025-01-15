6732b155ccd0742b7c95faef985e957cd11fa821
souhaibzarai <souhaibzarai18@gmail.com>
2025-01-15 22:45:59 +0100

    remove last changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,3 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp());
 }
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(Object context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-
-}

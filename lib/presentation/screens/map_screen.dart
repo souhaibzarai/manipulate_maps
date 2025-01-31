7139fcdca804f9b45d12c93a56ecaae66171adf2
souhaibzarai <souhaibzarai18@gmail.com>
2025-01-31 04:45:59 +0100

    working on map screen

--- lib/presentation/screens/map_screen.dart
+++ lib/presentation/screens/map_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:manipulate_maps/constants/strings.dart';
+import 'package:manipulate_maps/helpers/location_helper.dart';
+import 'package:manipulate_maps/presentation/widgets/auth_feature_button.dart';
 import '../../business_logic/cubit/phone_auth_cubit.dart';
-import '../../constants/strings.dart';
-import '../widgets/auth_feature_button.dart';
 import '../../constants/colors.dart';
 
 class MapScreen extends StatefulWidget {
@@ -38,32 +43,95 @@ void showProgressIndicator(BuildContext context) {
 class _MapScreenState extends State<MapScreen> {
   final _phoneAuthCubit = PhoneAuthCubit();
 
+  static Position? location;
+  final mapController = Completer<GoogleMapController>();
+
+  Future<void> get currentLocation async {
+    location = await LocationHelper.getcurrentPosition.whenComplete(() {
+      setState(() {});
+    });
+  }
+
+  static final CameraPosition myLocationCameraPosition = CameraPosition(
+    target: LatLng(
+      location!.latitude,
+      location!.longitude,
+    ),
+    zoom: 15,
+    bearing: 0.0,
+    tilt: 0.0,
+  );
+
+  Widget buildGoogleMap() {
+    return GoogleMap(
+      initialCameraPosition: myLocationCameraPosition,
+      mapType: MapType.normal,
+      myLocationEnabled: true,
+      zoomControlsEnabled: false,
+      myLocationButtonEnabled: false,
+      onMapCreated: (GoogleMapController controller) {
+        mapController.complete(controller);
+      },
+    );
+  }
+
+  Future<void> goToMyCurrentPosition() async {
+    final GoogleMapController controller = await mapController.future;
+
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(myLocationCameraPosition),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentLocation;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Welcome to map screen'),
-              SizedBox(height: 40),
-              BlocProvider<PhoneAuthCubit>(
+        body: Stack(
+          children: [
+            location != null
+                ? buildGoogleMap()
+                : Center(
+                    child: CircularProgressIndicator(
+                      color: AppColors.headerColor,
+                    ),
+                  ),
+            Positioned(
+              child: BlocProvider(
                 create: (context) => _phoneAuthCubit,
                 child: AuthFeatureButton(
-                  onClickEvent: () async {
-                    showProgressIndicator(context);
-                    await _phoneAuthCubit.logOut();
-                    if (!context.mounted) {
-                      return;
-                    }
-                    Navigator.pushReplacementNamed(context, authScreen);
-                  },
-                  alignment: Alignment.topCenter,
-                  text: 'Log Out',
-                ),
+                    alignment: Alignment.topCenter,
+                    onClickEvent: () async {
+                      showProgressIndicator(context);
+                      await _phoneAuthCubit.logOut();
+                      if (!context.mounted) {
+                        return;
+                      }
+                      Navigator.pushReplacementNamed(
+                        context,
+                        authScreen,
+                      );
+                    },
+                    text: 'Sign Out'),
               ),
-            ],
+            ),
+          ],
+        ),
+        floatingActionButton: Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 10, 30),
+          child: FloatingActionButton(
+            backgroundColor: AppColors.mainColor,
+            onPressed: goToMyCurrentPosition,
+            child: Icon(
+              Icons.place,
+              color: AppColors.thirdColor,
+            ),
           ),
         ),
       ),

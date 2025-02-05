3079bdb7685080015d62e27421f42ae3512080c2
souhaibzarai <souhaibzarai18@gmail.com>
2025-02-05 02:46:05 +0100

    api key edited

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,7 +1,7 @@
 const authScreen = '/auth-screen';
 const otpScreen = '/OTP-screen';
 const mapScreen = '/map-screen';
-const googleMapAPIKey = 'AIzaSyDFa6OVxlIgTUqk0YMK9vQ_6aAMRlsO2VQ';
+const googleMapAPIKey = 'AIzaSyADcmxcXARHYI6r3i3tV78A4Pa9-6hb3XM';
 const placesBaseUrl =
     'https://maps.googleapis.com/maps/api/place/queryautocomplete/json';
 const placeLocationBaseUrl =
